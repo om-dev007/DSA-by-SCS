dc9e2645111d0ee0cbd7373e62f65ac22652aa05
om-dev007 <officialomnarayan@gmail.com>
2025-09-17 21:25:38 +0530

    video 4 done

--- BasicsVideos/video4.js
+++ BasicsVideos/video4.js
@@ -55,13 +55,13 @@ let n = parseInt(prompt("Enter a num:- "));
 //     console.log();
 // }
 
-for(let i=1; i<=n; i++) {
-    for(let j=1; j<=n-i; j++) {
-        process.stdout.write("  ");
-    }
+// for(let i=1; i<=n; i++) {
+//     for(let j=1; j<=n-i; j++) {
+//         process.stdout.write("  ");
+//     }
 
-    for(let j=1; j<=i; j++) {
-        process.stdout.write("* ");
-    }
-    console.log();
-}
+//     for(let j=1; j<=i; j++) {
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+// }
