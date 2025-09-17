a1155d13a6b7c6e562c04c6c0463c1b89d96e841
om-dev007 <officialomnarayan@gmail.com>
2025-09-17 21:26:08 +0530

    v shape

--- BasicsVideos/video4.js
+++ BasicsVideos/video4.js
@@ -65,3 +65,15 @@ let n = parseInt(prompt("Enter a num:- "));
 //     }
 //     console.log();
 // }
+
+// for(let i=1; i<=n; i++) {
+//     for(let j=1; j<=2*n-1; j++) {
+//         if(j==i || (j+i) == (2*n)) {
+//             process.stdout.write(" * ")
+//         }
+//         else {
+//             process.stdout.write(" ")
+//         }
+//     }
+//     console.log();
+// }
