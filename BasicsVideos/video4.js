c5a4e8385726b760b81b9b76f79f564127583748
om-dev007 <officialomnarayan@gmail.com>
2025-09-17 17:55:26 +0530

    pattern apt

--- BasicsVideos/video4.js
+++ BasicsVideos/video4.js
@@ -18,9 +18,50 @@ let n = parseInt(prompt("Enter a num:- "));
 // }
 // console.log();
 
-for(let i = 0; i < n; i++) {
-    for(let j =1; j<=n; j++) {
-        process.stdout.write("* ")
+// for(let i = 0; i < n; i++) {
+//     for(let j =1; j<=n; j++) {
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }
+
+// for(let i=1; i<=n; i++) {
+//     for(let j=1; j<=i; j++) {
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }
+
+// for(let i = 1; i<=n; i++) {
+//     for(let j=1; j<=i; j++) {
+//         process.stdout.write(`${j} `)
+//     }
+//     console.log();
+// }
+
+// for(let i = 1; i<=n; i++) {
+//     let ascii = 65;
+//     for(let j=1; j<=i; j++) {
+//         process.stdout.write(String.fromCharCode(ascii)+ " ")
+//         ascii++
+//     }
+//     console.log();
+// }
+
+// for(let i=n; i>=1; i--) {
+//     for(let j=1; j<=i; j++) {
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }
+
+for(let i=1; i<=n; i++) {
+    for(let j=1; j<=n-i; j++) {
+        process.stdout.write("  ");
+    }
+
+    for(let j=1; j<=i; j++) {
+        process.stdout.write("* ");
     }
     console.log();
 }
