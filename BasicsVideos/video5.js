b83ce1d410b8c290532dbc473633f7625cec652e
om-dev007 <officialomnarayan@gmail.com>
2025-09-18 21:20:48 +0530

    Array apt

--- BasicsVideos/video5.js
+++ BasicsVideos/video5.js
@@ -84,17 +84,17 @@ let newArr = [12, 21, 42, 53, 10];
 
 // console.log(newArr);
 
-let arr = [1,1,0,1,0,1,1,0,0];
+// let arr = [1,1,0,1,0,1,1,0,0];
 
-let i=0, j=0;
-while(i<arr.length) {
-    if(arr[i] == 0) {
-        let temp = arr[i];
-        arr[i] = arr[j]
-        arr[j] = temp
-        j++
-    }
-    i++
-}
+// let i=0, j=0;
+// while(i<arr.length) {
+//     if(arr[i] == 0) {
+//         let temp = arr[i];
+//         arr[i] = arr[j]
+//         arr[j] = temp
+//         j++
+//     }
+//     i++
+// }
 
-console.log(arr);
+// console.log(arr);
