e7f215cdd74b8aad07547d4852a62f9e3cd1a7f8
om-dev007 <officialomnarayan@gmail.com>
2025-09-18 21:09:46 +0530

    video5 done

---
+++ BasicsVideos/video5.js
@@ -0,0 +1,100 @@
+let prompt = require('prompt-sync')()
+
+// let NewArr = []
+// let sum = 0
+// for(let i=0; i<5; i++) {
+//     let arr = parseInt(prompt("Enter a num:- "))
+//     NewArr.push(arr)
+// }
+
+// let max = NewArr[0]
+// let max1 = NewArr[1]
+// for(let i=0; i<NewArr.length; i++) {
+//     sum += NewArr[i]
+//     if(max < NewArr[i]) {
+//         max = NewArr[i]
+//     }
+//     else if (max > NewArr[i]) {
+//         max;
+//     }
+// }
+
+// console.log(NewArr);
+// console.log(sum);
+
+// console.log(max);
+
+// let newArr = [10, 10, 10, 10, 10];
+
+// let max1 = newArr[0]
+// let max2 = newArr[1]
+
+// for (let i = 0; i < newArr.length; i++) {
+//     if (max1 < newArr[i]) {
+//         max1 = newArr[i]
+//     }
+//     else {
+//         max1;
+//     }
+// }
+
+// for (let i = 0; i < newArr.length; i++) {
+//     if (newArr[i] == max1) {
+//         continue;
+//     }
+//     else if (max2 == newArr[i]) {
+//         max2
+//     }
+//     else if (max2 < newArr[i]) {
+//         max2 = newArr[i]
+//     }
+// }
+
+// if (max1 == max2) {
+//     console.log(`all the elements of the array is same so there is no wxist of max2 in your given array`);
+// }
+// else {
+//     console.log(max1);
+//     console.log(`Max2 = ${max2}`);
+// }
+
+let newArr = [12, 21, 42, 53, 10];
+
+// let reverseArr = [] ;
+
+// for(let i=newArr.length-1; i>=0; i--) {
+//     reverseArr.push(newArr[i]);
+// }
+
+// console.log(`Your given array is ${newArr} and reversed array is ${reverseArr}`);
+
+// console.log(newArr);
+
+
+// let i = 0; 
+// let j = newArr.length - 1;
+
+// while(i!=j) {
+//     let temp = newArr[i]
+//     newArr[i] = newArr[j]
+//     newArr[j] = temp;
+//     i++;
+//     j--;
+// }
+
+// console.log(newArr);
+
+let arr = [1,1,0,1,0,1,1,0,0];
+
+let i=0, j=0;
+while(i<arr.length) {
+    if(arr[i] == 0) {
+        let temp = arr[i];
+        arr[i] = arr[j]
+        arr[j] = temp
+        j++
+    }
+    i++
+}
+
+console.log(arr);
