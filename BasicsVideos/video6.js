5525231923cbde602517dd9772fff9e427a48399
om-dev007 <officialomnarayan@gmail.com>
2025-09-21 14:36:15 +0530

    done

--- BasicsVideos/video6.js
+++ BasicsVideos/video6.js
@@ -1,16 +1,26 @@
-let arr = [1,2,3,4,5]
+let arr = [53,12,64,15,86,55,87]
 
-let copy = arr[0]
+// let copy = arr[0]
 
 //  Left rotation by 1 in array elements
 
- for(let i=0; i<arr.length-1; i++) {
-    arr[i] = arr[i+1]
- }
+//  for(let i=0; i<arr.length-1; i++) {
+//     arr[i] = arr[i+1]
+//  }
 
- arr[arr.length-1] = copy;
+//  arr[arr.length-1] = copy;
 
-console.log(arr);
+// console.log(arr);
 
 // Right rotation by 1 in array elements
 
+let duplicate = arr[arr.length-1];
+
+for(let i=arr.length-1; i>0; i--) {
+    arr[i] = arr[i-1];
+    // console.log(`Arr at ${i} index is ${arr}`);
+}
+
+arr[0] = duplicate
+
+console.log(`New array is ${arr}`)
